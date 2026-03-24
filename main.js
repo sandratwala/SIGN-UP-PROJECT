3b0f99d9ace1319edfb5a22418f6bb318bc44311
sandra twala <sandraatwala@gmail.com>
2026-03-24 19:00:23 +0300

    added structure

---
+++ main.js
@@ -0,0 +1,12 @@
+document.getElementById('signupform').addEventListener('submit'. function(e) {
+    e.preventDefault();
+     
+    const username = document.getElementById('username').value:
+    const email = document.getElementById('email').value:
+    const password = document.getElementById('password').value:
+
+    if(username && email && password) {
+        alert('Account created successfully!');
+        this.resizeTo();
+    }
+});
